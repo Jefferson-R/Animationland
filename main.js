367f0c7a1d9dbc70d8681e048503ccc9e1244afa
Jefferson <radsjefferson@gmail.com>
2023-01-07 16:55:02 -0600

    v2.1.1

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let score = document.querySelector('.score > h4');
-let bunny1 = document.getElementById('bunny1');
+let bunny1 = document.querySelector('#bunny1');
 let bunny2 = document.querySelector('#bunny2');
 let bunny3 = document.querySelector('#bunny3');
 let bunny4 = document.querySelector('#bunny4');
@@ -12,7 +12,7 @@ function bunnyClicked (){
     score.innerHTML = `Score: ${total}`;
 }
 
-bunny1.addEventListener('click', bunnyClicked);
-bunny2.addEventListener('click', bunnyClicked);
-bunny3.addEventListener('click', bunnyClicked);
-bunny4.addEventListener('click', bunnyClicked);
+bunny1.addEventListener('click' || 'touch', bunnyClicked);
+bunny2.addEventListener('click' || 'touch', bunnyClicked);
+bunny3.addEventListener('click' || 'touch', bunnyClicked);
+bunny4.addEventListener('click' || 'touch', bunnyClicked);
